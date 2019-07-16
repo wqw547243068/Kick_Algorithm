e4fcbfd2f45d9a2c47e8270cee046948982f0d1a
wangqiwen <wqw547243068@163.com>
2019-07-16 21:50:03 +0800

    做过的leetcode题目

---
+++ pratice/integer_divide.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    """整数划分"""
+
+    def __init__(self):
+        pass
+
+    def integer_divide(self, a):
+        """
+            计算整数有多少种划分方式
+            递归公式：
+        """
+        pass
+
+
+if __name__ == '__main__':
+    a = 6
+    test = Solution()
+    test.integer_divide(a)
